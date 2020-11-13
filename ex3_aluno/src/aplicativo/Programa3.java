da6b27bc8ed11391fa119c3bbb38ebbbdcdc4c1b
RobsondCCarneiro <robinho.cenep@gmail.com>
2020-11-13 01:24:50 -0300

    Colocando construtor e encapsulamento no ex3

--- ex3_aluno/src/aplicativo/Programa3.java
+++ ex3_aluno/src/aplicativo/Programa3.java
@@ -10,19 +10,35 @@ public class Programa3 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Aluno aluno = new Aluno();
-		aluno.nome = sc.nextLine();
-		aluno.n1 = sc.nextDouble();
-		aluno.n2 = sc.nextDouble();
-		aluno.n3 = sc.nextDouble();
+		System.out.println("nome do aluno: ");
+		String nome = sc.nextLine();
+		System.out.println("nota 1: ");
+		double n1 = sc.nextDouble();
+		System.out.println("nota 2: ");
+		double n2 = sc.nextDouble();
+		System.out.println("nota 3: ");
+		double n3 = sc.nextDouble();
 		
-		System.out.printf("FINAL GRADE = %.2f\n", aluno.media());
-		if(aluno.media() < 60) {
-			System.out.printf("FAILED\nMISSING %.2f POINTS", 60.0 - aluno.media());
+		//Instanciando a variavel ponteiro "aluno" da classe "Aluno"
+		Aluno aluno = new Aluno(n1, n2, n3, nome);
+		
+		System.out.printf("NOTA FINAL = %.2f\n", aluno.media());
+		if(aluno.media() < 6.0) {
+			System.out.printf("REPROVADO\nFALTANDO %.2f PONTOS", 6.0 - aluno.media());
+			System.out.println();
 		}
 		else {
-			System.out.println("PASS");
+			System.out.println("APROVADO!");
 		}
+		
+		System.out.println("Testando o getN2: ");
+		System.out.println(aluno.getN2());
+		
+		System.out.println("Testando o setN1: ");
+		n1 = sc.nextDouble();
+		aluno.setN1(n1);
+		System.out.println("Saindo o valor de n1:");
+		System.out.println(aluno.getN1());
 		sc.close();
 
 	}
--- ex3_aluno/src/entidades/Aluno.java
+++ ex3_aluno/src/entidades/Aluno.java
@@ -1,9 +1,65 @@
 package entidades;
 
 public class Aluno {
-	public double n1, n2, n3;
-	public String nome;
+	private double n1, n2, n3;
+	private String nome;
 	
+	
+	//Construtor com parametros
+	public Aluno(double n1, double n2, double n3, String nome) {
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;
+		this.nome = nome;
+	}
+	
+	//Construtor padrao
+	public Aluno() {
+		
+	}
+
+
+	//Gets e Sets para capturar os valores quando quisesse acessar em qualquer area do codigo
+	public double getN1() {
+		return n1;
+	}
+	
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+
+	public double getN2() {
+		return n2;
+	}
+
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+
+	public double getN3() {
+		return n3;
+	}
+
+
+	public void setN3(double n3) {
+		this.n3 = n3;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
 	public double media(){
 		if(n1>=0.0 && n2>=0.0 && n3>=0.0 && n1<=100.0 && n2<=100.0 && n3<=100.0) {
 			//O problema original eh por soma, enquanto eu fiz por media ponderada
