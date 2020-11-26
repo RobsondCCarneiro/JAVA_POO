92caf9ee8cab38aa76bae04515d78b6d8c3766f2
RobsondCCarneiro <robinho.cenep@gmail.com>
2020-11-26 15:18:32 -0300

    Exemplo simples de SET no JAVA

---
+++ ex_Set/src/ex_Set/Main.java
@@ -0,0 +1,51 @@
+package ex_Set;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Sera que pode fazer uma Array de Set?
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
+		
+		System.out.println("Quantos estudantes do curso A: ");
+		int n = sc.nextInt();
+		for(int i = 1; i<=n; i++) {
+			System.out.println("Adicionando o estudante # " + i + " do curso A");
+			int numero = sc.nextInt();
+			a.add(numero);
+		}
+		
+		System.out.println("Quantos estudantes do curso B: ");
+		n = sc.nextInt();
+		for(int i = 1; i<=n; i++) {
+			System.out.println("Adicionando o estudante # " + i + " do curso B");
+			int numero = sc.nextInt();
+			b.add(numero);
+		}
+		
+		System.out.println("Quantos estudantes do curso C: ");
+		n = sc.nextInt();
+		for(int i = 1; i<=n; i++) {
+			System.out.println("Adicionando o estudante # " + i + " do curso C");
+			int numero = sc.nextInt();
+			c.add(numero);
+		}
+		
+		//a variavel total está  sendo instanciado pela classe HashSet que serva para 
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println("Total de estudantes: " + total.size());
+		sc.close();
+
+	}
+
+}
